38170aed39b46e82ad76cd7545d2ab2cfd63cd55
gregoirebourgeais-arch <gregoire.bourgeais@gmail.com>
2025-10-29 07:53:24 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "atelier-ppnc-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Installation du service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Activation et nettoyage des anciens caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.map((n) => n !== CACHE_NAME && caches.delete(n)))
+    )
+  );
+});
+
+// Interception des requêtes
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(
+      (response) =>
+        response ||
+        fetch(event.request).catch(() =>
+          caches.match("./index.html")
+        )
+    )
+  );
+});
